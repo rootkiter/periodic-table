c6ca68c4374471bd9f0e605443f1394122066be2
rootkiter <rootkiter@rootkiter.com>
2020-01-20 23:34:09 +0800

    try use js

---
+++ periodic.js
@@ -0,0 +1,32 @@
+var element_top = [
+    ["H", "Li", "Na", "Ka", "Rb", "Cs", "Fr"],
+    [     "Be", "Mg", "Ca", "Sr", "Ba", "Ra"],
+    [                 "Sc", "Y" , "La", "Ac"],
+    [                 "Ti", "Zr", "Hf", "Rf"],
+    [                 "V" , "Nb", "Ta", "Db"],
+    [                 "Cr", "Mo", "W" , "Sg"],
+    [                 "Mn", "Tc", "Re", "Bh"],
+    [                 "Fe", "Ru", "Os", "Hs"],
+    [                 "Co", "Rh", "Ir", "Mt"],
+    [                 "Ni", "Pd", "Pt", "Ds"],
+    [                 "Cu", "Ag", "Au", "Rg"],
+    [                 "Zn", "Cd", "Hg", "Uub"],
+    [     "B" , "Al", "Ga", "In", "Tl", "Uut"],
+    [     "C" , "Si", "Ge", "Sn", "Pb", "Uuq"],
+    [     "N" , "P" , "As", "Sb", "Bi", "Uup"],
+    [     "O" , "S" , "Se", "Te", "Po", "Uuh"],
+    [     "F" , "Cl", "Br", "I" , "At", "Uus"],
+    ["He","Ne", "Ar", "Kr", "Xe", "Rn", "Uuo"]
+];
+
+var rare = [
+    [
+        "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu",
+        "Gd", "Tb", "Dy", "Ho", "Tm", "Yb", "Lu"
+    ],
+    [
+        "Ac", "Th", "Pa", "U" , "Np", "Pu", "Am",
+        "Cm", "Bk", "Cf", "Es", "Mc", "No", "Lr"
+    ]
+];
+
